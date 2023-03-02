963c444f898fef2320a66742ee5f011aaa1be362
Victor Silva <victorhugodasilva26@gmail.com>
2023-03-01 21:47:58 -0300

    01/03/2023 - v0.2

---
+++ javascript/test.js
@@ -0,0 +1,11 @@
+var test = document.getElementById("teste");
+
+test.addEventListener("mouseover", function( event ) {
+    // highlight the mouseover target
+    event.target.style.color = "orange";
+
+    // reset the color after a short delay
+    setTimeout(function() {
+      event.target.style.color = "";
+    }, 500);
+  }, false);
