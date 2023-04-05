c2cb8a7f600ee01f65866310285e63f98c6c0a40
Victor Silva <victorhugodasilva26@gmail.com>
2023-04-05 01:27:29 -0300

    04/04/23
    - Passado para bootstrap
    - Melhorias de Eficiencia
    - Funcionalidade das setas do slide-show

--- javascript/switcher.js
+++ javascript/switcher.js
@@ -1,20 +1,41 @@
 var Index = 0;
+var TimerID = 0;
 
-Atualizar_Switcher();
+Atualizar_Switcher(0);
 
-function Atualizar_Switcher(){
-    var imagens = document.getElementsByClassName("switcher-images");
-    
-    for(var i=0;i < imagens.length; i++){
-        imagens[i].style.display = "none";
+function Atualizar_Switcher(forcado) {
+    var imagens = document.querySelectorAll('.carousel-item');
+    var indicators = document.querySelectorAll('.carousel-indicators li');
+
+    if (TimerID) {
+        clearTimeout(TimerID);
+        TimerID = 0;
     }
 
-    Index++;
+    imagens.forEach(function (imagem) {
+        imagem.classList.remove('active');
+    });
 
-    if(Index > imagens.length){
+
+    (forcado == 2) ? Index-- : Index++;
+
+    if (Index < 1) {
+        Index = imagens.length;
+    } else if (Index > imagens.length) {
         Index = 1;
     }
 
-    imagens[Index-1].style.display = "block";
-    setTimeout(Atualizar_Switcher,5000)
+    imagens[Index - 1].classList.add('active');
+
+    indicators.forEach(function (indicator) {
+        indicator.classList.remove('active');
+    });
+
+    indicators[Index - 1].classList.add('active');
+
+    if (forcado > 0) {
+        TimerID = setTimeout(Atualizar_Switcher, 25000, 0);
+    } else {
+        TimerID = setTimeout(Atualizar_Switcher, 2000, 0);
+    }
 }
