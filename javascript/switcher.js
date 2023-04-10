ecf9daa45355b603d6f87683dbfa6802c2da1573
Victor Silva <victorhugodasilva26@gmail.com>
2023-04-09 21:40:32 -0300

    09/05/23
    Adicionado:
    - Footer;
    - Sections;
    - Números.

--- javascript/switcher.js
+++ javascript/switcher.js
@@ -1,41 +1,56 @@
-var Index = 0;
-var TimerID = 0;
+/*  Atualização 05/04/2023
+    - Otimização de código
+*/
 
-Atualizar_Switcher(0);
+let index = 0;
+let timerId = setInterval(atualizarSwitcher, 2000);
 
-function Atualizar_Switcher(forcado) {
+function atualizarSwitcher(forcado = 0) {
+    /*
+        forcado = 0 -> Atualização 'natural'
+        forcado = 1 -> Click na seta de 'avançar'
+        forcado = 2 -> Click na seta de 'recuar'
+    */
+
+    // Seleção dos elementos através de um array
     var imagens = document.querySelectorAll('.carousel-item');
     var indicators = document.querySelectorAll('.carousel-indicators li');
 
-    if (TimerID) {
-        clearTimeout(TimerID);
-        TimerID = 0;
-    }
+    // Reset das classes bootstraap
+    /* ------------------ Codigo antigo ------------------
+        for(var i=0; i < imagens.lenght; i++){
+            imagens[i].classlist.remove('active');
+        }
+        for(var i=0; i < indicators.lenght; i++){
+            indicators[i].classlist.remove('active');
+        }
 
-    imagens.forEach(function (imagem) {
-        imagem.classList.remove('active');
-    });
+    */
+    imagens.forEach((imagem) => imagem.classList.remove('active'));
+    indicators.forEach((indicator) => indicator.classList.remove('active'));
 
+    // Verificação posição index + tipo de parâmetro
+    /* ------------------ Codigo antigo ------------------
+        (forcado == 2) ? Index-- : Index++;
 
-    (forcado == 2) ? Index-- : Index++;
+        if (Index < 1) {
+            Index = imagens.length;
+        } else if (Index > imagens.length) {
+            Index = 1;
+        }  
+    */
+    index = (index + (forcado === 2 ? -1 : 1) + imagens.length) % imagens.length;
 
-    if (Index < 1) {
-        Index = imagens.length;
-    } else if (Index > imagens.length) {
-        Index = 1;
-    }
+    // Atribuição da classe bootstraap aos elementos ativos
+    imagens[index].classList.add('active');
+    indicators[index].classList.add('active');
 
-    imagens[Index - 1].classList.add('active');
-
-    indicators.forEach(function (indicator) {
-        indicator.classList.remove('active');
-    });
-
-    indicators[Index - 1].classList.add('active');
-
-    if (forcado > 0) {
-        TimerID = setTimeout(Atualizar_Switcher, 25000, 0);
-    } else {
-        TimerID = setTimeout(Atualizar_Switcher, 2000, 0);
-    }
+    // Set dos timers automaticos:
+    /*
+        forcado = 0 -> 5s = 5000ms
+        forcado = 1 -> 25s = 25000ms
+        forcado = 2 -> 25s = 25000ms
+    */
+    clearInterval(timerId);
+    timerId = setInterval(atualizarSwitcher, forcado ? 25000 : 5000);
 }
