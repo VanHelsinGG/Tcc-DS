ca119b095be30e932f7c02a80c679aae5014e7fb
Victor Silva <victorhugodasilva26@gmail.com>
2023-04-13 22:40:15 -0300

    13/04/2023
    - Imagens rotativas no slide-show
    - Otimização das sections

---
+++ javascript/main.js
@@ -0,0 +1,5 @@
+direitosAutorais();
+
+function direitosAutorais() {
+    document.getElementById("data").textContent = new Date().getFullYear();
+}
--- javascript/switcher.js
+++ javascript/switcher.js
@@ -1,5 +1,8 @@
 /*  Atualização 05/04/2023
     - Otimização de código
+
+    Atualização 13/04/2023
+    - Adição de textos rotativos
 */
 
 let index = 0;
@@ -15,35 +18,28 @@ function atualizarSwitcher(forcado = 0) {
     // Seleção dos elementos através de um array
     var imagens = document.querySelectorAll('.carousel-item');
     var indicators = document.querySelectorAll('.carousel-indicators li');
+    var titulo = document.getElementById("image-title");
+    const textos = ["Atividades físicas para prevenir doenças!", 
+    "Acompanhe seu progresso com facilidade!", 
+    "Aprenda enquanto se exercita!", 
+    "Alimentação equilibrada para uma vida saudável!", 
+    "Ganhe pontos e recompensas por se exercitar!", 
+    "Monitore sua saúde com a calculadora IMC!", 
+    "Cuide de corpo e mente com hábitos saudáveis!"];
 
     // Reset das classes bootstraap
-    /* ------------------ Codigo antigo ------------------
-        for(var i=0; i < imagens.lenght; i++){
-            imagens[i].classlist.remove('active');
-        }
-        for(var i=0; i < indicators.lenght; i++){
-            indicators[i].classlist.remove('active');
-        }
-
-    */
     imagens.forEach((imagem) => imagem.classList.remove('active'));
     indicators.forEach((indicator) => indicator.classList.remove('active'));
 
     // Verificação posição index + tipo de parâmetro
-    /* ------------------ Codigo antigo ------------------
-        (forcado == 2) ? Index-- : Index++;
-
-        if (Index < 1) {
-            Index = imagens.length;
-        } else if (Index > imagens.length) {
-            Index = 1;
-        }  
-    */
     index = (index + (forcado === 2 ? -1 : 1) + imagens.length) % imagens.length;
 
     // Atribuição da classe bootstraap aos elementos ativos
     imagens[index].classList.add('active');
     indicators[index].classList.add('active');
+
+    //Rotatividade dos textos
+    titulo.textContent = textos[index];
 
     // Set dos timers automaticos:
     /*
