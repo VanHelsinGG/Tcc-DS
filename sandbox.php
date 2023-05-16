0684dafc4a3d48c6581c318324bd91bd4ed35ed0
VanHelsinGG <88602273+VanHelsinGG@users.noreply.github.com>
2023-05-16 13:58:40 -0300

    16/05/23

--- sandbox.php
+++ sandbox.php
@@ -6,79 +6,131 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <script src="./javascript/mainUser.js" defer></script>
     <link rel="stylesheet" href="./style/universal.css">
     <link rel="stylesheet" href="./style/main.css">
     <link rel="stylesheet" href="./images/bootstrap-icons-1.10.4/font/bootstrap-icons.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 </head>
 
-<body style="background-color: #ca7f16;">
+<body style="background-color:#e9e9e9;">
 
     <div class="container-fluid">
         <div class="row">
-            <nav class="col-md-2 col-lg-2 d-md-block escuro text-white vh-100 sidebar">
+            <nav class="col-md-2 col-lg-2 d-md-block escuro text-white vh-100 sidebar" id="navbar">
+                <!-- <img src="./images/logotitle.png" alt="" class="img-fluid"> -->
                 <ul class="list-unstyled list-group h-100 pt-4">
                     <li class="active my-1"><a href="#" class="text-white btn my-2"><i class="bi bi-house-fill me-3"></i>Inicio</a></li>
                     <li class="my-1"><a href="#" class="text-white btn my-2"><i class="bi bi-bullseye me-3"></i>Exercicios</a></li>
                     <li class="my-1"><a href="#" class="text-white btn my-2"><i class="bi bi-basket3-fill me-3"></i>Dieta</a></li>
                     <li class="my-1"><a href="#" class="text-white btn my-2"><i class="bi bi-clipboard2-data-fill me-3"></i>Relatorios</a></li>
-                    <li class="mt-auto my-1"><a href="" class="text-white btn my-2 fs-5"><i class="bi bi-person-circle me-3"></i>Olympia Workout</a></li>
+                    <li class="mt-auto my-1" style="border-top: 1px solid #363330;"><a href="" class="text-white btn my-2"><i class="bi bi-person-circle me-3"></i>Olympia Workout</a></li>
                 </ul>
+
             </nav>
-            
-            <main class="col-10 ms-sm-auto col-lg-10 px-md-4">
-                
-                <div class="container-fluid mt-5">
-                    <div class="row bg-dark w-auto">
-                        <p class="text-white">Seus dados</p>
+
+            <main class="col-10 ms-auto col-lg-10 bg-dark vh-100">
+                <div class="container-fluid">
+                    <div class="row text-white bg-laranja p-2">
+                        <h3>Confira nossos principais serviços</h3>
                     </div>
-                    <div class="row">
-                        <div class="col-4 bg-danger">
-                            <div class="row bg-primary">
-                                <div class="col-6">
-                                    {imagem de perfil}
-                                </div>
-                                <div class="col-6">
-                                    {nome}
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-12 bg-success">
-                                    Peso:<br>
-                                    Altura:<br>
-                                    Imc:<br>
-                                    Ultimo treino:
-                                </div>
-                            </div>
+                    <div class="row action text-white">
+                        <div class="col-4 db d-flex align-items-end actions">
+                            <h4>Database</h4>
                         </div>
-                        <div class="col-4 bg-warning">
-                            Próximos treinos
+                        <div class="col-4 dieta d-flex align-items-end actions">
+                            <h4>Database</h4>
                         </div>
-                        <div class="col-4 bg-secondary">
-                            Seus dados semanais
+                        <div class="col-4 relatorios d-flex align-items-end actions">
+                            <h4>Database</h4>
                         </div>
                     </div>
                 </div>
 
-                <div class="container-fluid mt-5">
-                    <div class="row bg-dark text-white">
-                        Tops
-                    </div>
+
+                <!-- <div class="container-fluid mt-5 p-4 bg-dark-emphasis">
                     <div class="row">
-                        <div class="col-4 bg-success">
-                            Diario
+                        <div class="col-4">
+                            <h3 class="text-secondary"><i class="bi bi-arrow-right me-2"></i>Top Diario</h3>
+                            <table class="table-dark table-striped table text-center">
+                                <tr>
+                                    <th>{posição}</th>
+                                    <th>{nome}</th>
+                                    <th>{tempo diario}</th>
+                                </tr>
+                                <tr>
+                                    <th>1</th>
+                                    <td>Victor</td>
+                                    <td>10 minutos</td>
+                                </tr>
+                                <tr>
+                                    <th>2</th>
+                                    <td>Kauê</td>
+                                    <td>12 minutos</td>
+                                </tr>
+                                <tr>
+                                    <th>...</th>
+                                    <td>...</td>
+                                    <td>...</td>
+                                </tr>
+                            </table>
                         </div>
-                        <div class="col-4 bg-danger">
-                            Semanal
+                        <div class="col-4">
+                            <h3 class="text-secondary"><i class="bi bi-arrow-right me-2"></i>Top Semanal</h3>
+                            <table class="table-dark table-striped table text-center">
+                                <tr>
+                                    <th>{posição}</th>
+                                    <th>{nome}</th>
+                                    <th>{tempo diario}</th>
+                                </tr>
+                                <tr>
+                                    <th>1</th>
+                                    <td>Victor</td>
+                                    <td>10 minutos</td>
+                                </tr>
+                                <tr>
+                                    <th>2</th>
+                                    <td>Kauê</td>
+                                    <td>12 minutos</td>
+                                </tr>
+                                <tr>
+                                    <th>...</th>
+                                    <td>...</td>
+                                    <td>...</td>
+                                </tr>
+                            </table>
                         </div>
-                        <div class="col-4 bg-primary">
-                            Mensal
+                        <div class="col-4">
+                            <h3 class="text-secondary"><i class="bi bi-arrow-right me-2"></i>Top Mensal</h3>
+                            <table class="table-dark table-striped table text-center">
+                                <tr>
+                                    <th>{posição}</th>
+                                    <th>{nome}</th>
+                                    <th>{tempo diario}</th>
+                                </tr>
+                                <tr>
+                                    <th>1</th>
+                                    <td>Victor</td>
+                                    <td>10 minutos</td>
+                                </tr>
+                                <tr>
+                                    <th>2</th>
+                                    <td>Kauê</td>
+                                    <td>12 minutos</td>
+                                </tr>
+                                <tr>
+                                    <th>...</th>
+                                    <td>...</td>
+                                    <td>...</td>
+                                </tr>
+                            </table>
                         </div>
                     </div>
-                </div>
+                </div> -->
             </main>
         </div>
     </div>
+    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </body>
 
